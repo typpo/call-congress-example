8b123bc4def742ff99d06e2cdb2e30c28949eb9c
Ian Webster <ianw_git@ianww.com>
2017-04-25 15:09:08 -0700

    Convert functions to es6 fat arrows

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 // Twilio will load this route when you receive a new call.
-app.post('/new_phone_call', function(req, res) {
+app.post('/new_phone_call', (req, res) => {
   // Construct our phone response.
   const call = new twilio.TwimlResponse();
   call.say('Thanks for calling.  Enter your zip code to be connected with your senators and representatives.');
@@ -24,15 +24,15 @@ app.post('/new_phone_call', function(req, res) {
   res.status(200).type('text/xml').send(call.toString());
 });
 
-app.post('/redirect_to_congress', function(req, res) {
+app.post('/redirect_to_congress', (req, res) => {
   // Get the zip code digits inputted by the user.
   const zip = req.body.Digits;
-  request('https://congress.api.sunlightfoundation.com/legislators/locate?apikey=YOUR_API_KEY_HERE&zip=' + zip, function(err, resp, body) {
+  request('https://congress.api.sunlightfoundation.com/legislators/locate?apikey=YOUR_API_KEY_HERE&zip=' + zip, (err, resp, body) => {
     // A list of objects that represent members of Congress.
     const people = JSON.parse(body).results;
 
     const call = new twilio.TwimlResponse();
-    people.forEach(function(person) {
+    people.forEach((person) => {
       const name = person.first_name + ' ' + person.last_name;
       call.say('Now connecting you with ' + name);
       call.dial(person.phone);
